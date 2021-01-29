17238e3d8fda04f2116b5fe37018983a394187d1
Sergiu Buhatel <sergiubuhatel@Sergius-MacBook-Pro-2.local>
2021-01-29 10:04:43 -0500

    Keycloak

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from onlinedatabase_api.web.views import *
-from onlinedatabase_api import onlinedatabase_factory
+from flask_seed_api.web.views import *
+from flask_seed_api import flask_seed_factory
 
 global app
 
-app = onlinedatabase_factory.create_app(__name__)
+app = flask_seed_factory.create_app(__name__)
 app.app_context().push()
-onlinedatabase_factory.register_blueprints(app)
+flask_seed_factory.register_blueprints(app)
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=7022)
